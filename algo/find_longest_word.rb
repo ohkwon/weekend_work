d39104e7f0a44399455c0262b9781a59d1647bbf
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-15 19:04:39 -0700

    complete find_longest_word algorithm problem

--- algo/find_longest_word.rb
+++ algo/find_longest_word.rb
@@ -2,6 +2,18 @@
 
 def find_longest_word(sentence)
 
+  sentence_array = sentence.split(/\W+/)
+
+  longest_word = sentence_array[0]
+
+  sentence_array.each do |word|
+    if word.length > longest_word.length
+      longest_word = word
+    end
+  end
+
+  return longest_word
+
 end
 
 # Driver code - don't touch anything below this line.
